7bef2c2aeb57d9c9a2658e3bef26de295b855255
S. Krauss <silvio.krauss@doz.abbts.ch>
2024-09-10 15:19:28 +0200

    modifications for jupyterbook

---
+++ helper.bat
@@ -0,0 +1,6 @@
+robocopy 01_BASICS\IMG _build\html\01_BASICS\IMG /mir
+robocopy 01_BASICS\DATA _build\html\01_BASICS\DATA /mir
+
+robocopy 02_RGB_MATRIX\IMG _build\html\02_RGB_MATRIX\IMG /mir
+robocopy 02_RGB_MATRIX\DATA _build\html\02_RGB_MATRIX\DATA /mir
+robocopy 02_RGB_MATRIX\VIDEOS _build\html\02_RGB_MATRIX\VIDEOS /mir
